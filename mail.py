cdeaf06eaea0c06ad5d3584224f356f703725f28
Julien <julienremant95@Gmail.com>
2021-12-16 17:23:02 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,17 +1,19 @@
 import smtplib
+from datetime import datetime
 
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_USERNAME = "veridis95@gmail.com"
-SMTP_PASSWORD = "Cf38vg93?!"
-EMAIL_FROM = "veridis95@gmail.com"
-EMAIL_TO = "julienremant95@gmail.com"
-EMAIL_SUBJECT = "Alerte Veridis"
-EMAIL_MESSAGE = "Attention, le module de surveillance a detecte une presence !"
+def envoi():
+    SMTP_SERVER = "smtp.gmail.com"
+    SMTP_PORT = 587
+    SMTP_USERNAME = "veridis95@gmail.com"
+    SMTP_PASSWORD = "Cf38vg93?!"
+    EMAIL_FROM = "veridis95@gmail.com"
+    EMAIL_TO = "julienremant95@gmail.com"
+    EMAIL_SUBJECT = "Alerte Veridis"
+    EMAIL_MESSAGE = f"Attention, le module de surveillance a detecte une presence !\nDerniere intrusion : {datetime.now()}"
 
-s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-s.starttls()
-s.login(SMTP_USERNAME, SMTP_PASSWORD)
-message = 'Subject: {}\n\n{}'.format(EMAIL_SUBJECT, EMAIL_MESSAGE)
-s.sendmail(EMAIL_FROM, EMAIL_TO, message)
-s.quit()
+    s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    s.starttls()
+    s.login(SMTP_USERNAME, SMTP_PASSWORD)
+    message = 'Subject: {}\n\n{}'.format(EMAIL_SUBJECT, EMAIL_MESSAGE)
+    s.sendmail(EMAIL_FROM, EMAIL_TO, message)
+    s.quit()
